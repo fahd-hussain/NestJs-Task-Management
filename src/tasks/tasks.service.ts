255ec3cf7a5dbca2cd5d74417f9c7ef874abb7b9
Fahad Hussain <fahd__hussain@hotmail.com>
2021-05-19 01:04:14 +0500

    Not found on deleting task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -57,7 +57,9 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const found = this.getTaskById(id);
+
+    this.tasks = this.tasks.filter((task) => task.id !== found.id);
   }
 
   updateStatus(id: string, status: TaskStatus): Task {
